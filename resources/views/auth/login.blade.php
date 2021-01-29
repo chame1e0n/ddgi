34a023983bbc9e9b3bed9565bde17e2a6b62d253
Bobur Zaitov <bobur_moscow@mail.ru>
2021-01-29 11:01:14 +0200

    changed login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,12 +21,12 @@
 <body class="hold-transition login-page">
 <div class="login-box">
     <div class="login-logo">
-        <a href="/assets/index2.html"><b>Admin</b>LTE</a>
+        <a href="/assets/index2.html"><b>DDGI</b></a>
     </div>
     <!-- /.login-logo -->
     <div class="card">
         <div class="card-body login-card-body">
-            <p class="login-box-msg">Sign in to start your session</p>
+            <p class="login-box-msg"></p>
 
             <form method="POST" action="{{ route('login') }}">
                 @csrf
@@ -45,7 +45,7 @@
                     </div>
                 </div>
                 <div class="input-group mb-3">
-                    <input type="password" class="form-control" name="password" required  placeholder="Password">
+                    <input type="password" class="form-control" name="password" required  placeholder="Пароль">
                     @error('password')
                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -60,7 +60,7 @@
                 <div class="row">
                     <!-- /.col -->
                     <div class="col-4">
-                        <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                        <button type="submit" class="btn btn-primary btn-block">Войти</button>
                     </div>
                     <!-- /.col -->
                 </div>
