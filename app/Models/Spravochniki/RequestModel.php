78d394da1588926645f41d2bf4bdc500631740d7
baxt1yor <0898baxti@mail.ru>
2021-02-13 00:24:52 +0500

    RequestModel hasOne relationship

--- app/Models/Spravochniki/RequestModel.php
+++ app/Models/Spravochniki/RequestModel.php
@@ -4,6 +4,8 @@ namespace App\Models\Spravochniki;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\User;
+use App\Models\Policy;
 
 class RequestModel extends Model
 {
@@ -20,4 +22,14 @@ class RequestModel extends Model
 			"policy_transfer" => "Прием-передача полисов", 
 			"underwritting" =>"Андеррайтинг"
 	];
+
+	public function user()
+	{
+		return $this->hasone(User::class, 'id', 'from_whom');
+	}
+
+	public function policy()
+	{
+		return $this->hasOne(Policy::class, 'id', 'policy_blank');
+	}
 }
