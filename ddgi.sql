c05a22fdb65ae418fb5d581d3e1bcc5993fbe77b
Bobur Zaitov <bobur_moscow@mail.ru>
2021-04-22 23:05:09 +0300

    added filter to policyFlow index page

--- ddgi.sql
+++ ddgi.sql
@@ -2885,8 +2885,8 @@ CREATE TABLE IF NOT EXISTS `policy_flows` (
   `branch_id` int(11) DEFAULT NULL,
   `to_user_id` int(11) DEFAULT NULL,
   `from_user_id` int(11) DEFAULT NULL,
-  `policy_from` varchar(100) DEFAULT NULL,
-  `policy_to` varchar(100) DEFAULT NULL,
+  `policy_from` int(11) DEFAULT NULL,
+  `policy_to` int(11) DEFAULT NULL,
   `polis_name` varchar(100) DEFAULT NULL,
   `price_per_policy` varchar(100) DEFAULT NULL,
   `polis_given_by_user_id` int(11) unsigned DEFAULT NULL,
@@ -2900,13 +2900,13 @@ CREATE TABLE IF NOT EXISTS `policy_flows` (
 DELETE FROM `policy_flows`;
 /*!40000 ALTER TABLE `policy_flows` DISABLE KEYS */;
 INSERT INTO `policy_flows` (`id`, `act_number`, `act_date`, `a_reg`, `status`, `branch_id`, `to_user_id`, `from_user_id`, `policy_from`, `policy_to`, `polis_name`, `price_per_policy`, `polis_given_by_user_id`, `created_at`, `updated_at`, `deleted_at`) VALUES
-	(6, 'asdvvd', '2021-04-15', 'on', 'registered', NULL, 3, NULL, '1', '30', NULL, NULL, NULL, '2021-04-20 00:00:00', '2021-04-20 00:00:00', NULL),
-	(7, 'asdvvd', '2021-04-01', 'on', 'registered', NULL, 3, NULL, '100', '101', NULL, NULL, NULL, '2021-04-20 00:00:00', '2021-04-20 00:00:00', NULL),
-	(8, 'asdvvd', '2021-04-15', 'a4', 'registered', NULL, 3, NULL, '141', '142', 'Some name', '5444', NULL, '2021-04-20 00:00:00', '2021-04-20 00:00:00', NULL),
-	(9, 'gdfff', '2021-04-16', 'a5', 'registered', NULL, 3, NULL, '1000', '1005', 'Наименование', '4000', NULL, '2021-04-21 00:00:00', '2021-04-21 00:00:00', NULL),
-	(11, 'vsdvfdfsv', '2021-04-21', 'a4', 'transferred', NULL, 3, NULL, '1000', '1001', 'Наименование', '4000', NULL, '2021-04-22 00:00:00', '2021-04-22 00:00:00', NULL),
-	(12, 'asdvvd', '2021-04-16', 'a4', 'transferred', 1, 9, 3, '1002', '1003', 'dfgbdfbg', '4000', NULL, '2021-04-22 00:00:00', '2021-04-22 00:00:00', NULL),
-	(13, 'asdvvd', '2021-04-22', 'a4', 'retransferred', 1, 4, 9, '1000', '1001', 'dfgbdfbg', '4000', NULL, '2021-04-22 00:00:00', '2021-04-22 00:00:00', NULL);
+	(6, 'asdvvd', '2021-04-15', 'on', 'registered', NULL, 3, NULL, 1, 30, NULL, NULL, NULL, '2021-04-20 00:00:00', '2021-04-20 00:00:00', NULL),
+	(7, 'asdvvd', '2021-04-01', 'on', 'registered', NULL, 3, NULL, 100, 101, NULL, NULL, NULL, '2021-04-20 00:00:00', '2021-04-20 00:00:00', NULL),
+	(8, 'asdvvd', '2021-04-15', 'a4', 'registered', NULL, 3, NULL, 141, 142, 'Some name', '5444', NULL, '2021-04-20 00:00:00', '2021-04-20 00:00:00', NULL),
+	(9, 'gdfff', '2021-04-16', 'a5', 'registered', NULL, 3, NULL, 1000, 1005, 'Наименование', '4000', NULL, '2021-04-21 00:00:00', '2021-04-21 00:00:00', NULL),
+	(11, 'vsdvfdfsv', '2021-04-21', 'a4', 'transferred', NULL, 3, NULL, 1000, 1001, 'Наименование', '4000', NULL, '2021-04-22 00:00:00', '2021-04-22 00:00:00', NULL),
+	(12, 'asdvvd', '2021-04-16', 'a4', 'transferred', 1, 9, 3, 1002, 1003, 'dfgbdfbg', '4000', NULL, '2021-04-22 00:00:00', '2021-04-22 00:00:00', NULL),
+	(13, 'asdvvd', '2021-04-22', 'a4', 'retransferred', 1, 4, 9, 1000, 1001, 'dfgbdfbg', '4000', NULL, '2021-04-22 00:00:00', '2021-04-22 00:00:00', NULL);
 /*!40000 ALTER TABLE `policy_flows` ENABLE KEYS */;
 
 -- Dumping structure for table ddgi_test.policy_flow_files
