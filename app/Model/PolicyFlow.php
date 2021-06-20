3a42189b3e752e70638da7d8cda0c1b574b56698
Bobur Zaitov <bobur_moscow@mail.ru>
2021-06-20 14:15:14 +0300

    fixed PolicyRegistrationController. Added function from PolicyFlow Model

--- app/Model/PolicyFlow.php
+++ app/Model/PolicyFlow.php
@@ -2,11 +2,8 @@
 
 namespace App\Model;
 
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Request;
 
 class PolicyFlow extends Model
 {
@@ -84,35 +81,5 @@ class PolicyFlow extends Model
     public function policy_flow_files()
     {
         return $this->hasMany(PolicyFlowFile::class);
-    }
-
-    /**
-     * Create new policies in policies table and register them in policy_flows table
-     * @param Request $request
-     */
-    static function createNewPoliciesAndPolicyFlows(Request $request)
-    {
-        $createdByUserId = Auth::user()->employees->first()->id;
-
-        for ($i = $request->policy_from; $i <= $request->policy_to; $i++) {
-            $policy = new Policy;
-            $policy->series = $i;
-            $policy->act_number = $request->act_number;
-            $policy->print_size = $request->a_reg;
-            $policy->name = $request->polis_name;
-            $policy->price = $request->price_per_policy;
-            $policy->employee_id = $createdByUserId;
-            $policy->status = self::STATUS_REGISTERED;
-            $policy->date_of_issue = Carbon::now();
-            $policy->save();
-
-            PolicyFlow::create([
-                'act_date' => $request->act_date,
-                'policy_id' => $policy->id,
-                'policy_given_by_employee_id' => $createdByUserId,
-                'branch_id' => $request->branch_id,
-                'status' => self::STATUS_REGISTERED,
-            ]);
-        }
     }
 }
