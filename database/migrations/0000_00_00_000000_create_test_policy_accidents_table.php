8df77ff9e9e92a7c9cd85cf313b7835575bf0a13
Vitalii Zinkov <chameleon2803@gmail.com>
2021-07-12 20:20:16 +0600

    Correcting DB scheme.

--- database/migrations/0000_00_00_000000_create_test_policy_accidents_table.php
+++ database/migrations/0000_00_00_000000_create_test_policy_accidents_table.php
@@ -15,12 +15,12 @@ class CreateTestPolicyAccidentsTable extends Migration
     {
         Schema::create('policy_accidents', function (Blueprint $table) {
             $table->increments('id');
-            $table->tinyInteger('is_chronic_disease')->unsigned()->default(0);
             $table->string('fio', 45);
             $table->date('birthdate');
             $table->string('passport_series', 45);
             $table->string('passport_number', 45);
-            $table->string('beneficiary_fio', 45);
+            $table->string('beneficiary', 45);
+            $table->tinyInteger('is_chronic_disease')->unsigned()->default(0);
             $table->timestamps();
             $table->timestamp('deleted_at')->nullable();
         });
