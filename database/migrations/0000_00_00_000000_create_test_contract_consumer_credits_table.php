f1efe4ff31918aab17195753b059e821cfd094cd
Vitalii Zinkov <chameleon2803@gmail.com>
2021-08-14 13:05:01 +0600

    Correcting data base structure for the new requirements.

--- database/migrations/0000_00_00_000000_create_test_contract_consumer_credits_table.php
+++ database/migrations/0000_00_00_000000_create_test_contract_consumer_credits_table.php
@@ -15,20 +15,15 @@ class CreateTestContractConsumerCreditsTable extends Migration
     {
         Schema::create('contract_consumer_credits', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('loan_agreement');
-            $table->string('insurance_agreement_number', 45);
-            $table->date('insurance_agreement_date');
             $table->string('credit_agreement_number', 45);
             $table->date('credit_agreement_date');
             $table->date('credit_from');
             $table->date('credit_to');
-            $table->date('validity_from');
-            $table->date('validity_to');
+            $table->float('credit_sum', 12, 3)->unsigned();
             $table->string('collateral_type', 45);
             $table->string('purpose')->nullable();
             $table->string('description', 45);
             $table->float('collateral_sum', 12, 3)->unsigned();
-            $table->string('other_security_forms')->nullable();
             $table->timestamps();
             $table->timestamp('deleted_at')->nullable();
         });
